96809651462a4553947450bcb7307c66bbd8b774
David Oluseun <a.davidoluseun@gmail.com>
2020-07-11 01:07:26 +0100

    push local changes

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@
     
         // Use smooth scrolling when clicking on navigation
 
-        var topoffset = 54; //variable for menu height
+        var topoffset = 60; // Variable for menu height
   
-        $(".navbar-brand a, .navbar-nav a").click(function() {
+        $(".navbar-brand a, .navbar-nav a, .btn-hire").click(function() {
             if (location.pathname.replace(/^\//,"") ===
                 this.pathname.replace(/^\//,"") &&
                 location.hostname === this.hostname) {
@@ -27,10 +27,24 @@
                         return false;
 
                     } //target.length
-
-            } //click function
+            } 
 
         }); //smooth scrolling
+
+        // Bootstrap form validation
+        // Fetch the form to apply custom Bootstrap validation style to
+        var forms = document.getElementsByClassName("contact-form");
+
+        // Loop over it and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener("submit", function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add("was-validated");
+            }, false);
+        });
   
     }); // jQuery document ready function
 
