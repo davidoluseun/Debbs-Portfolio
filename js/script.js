1c919bce7620f19b498663898b29b51592f861b2
David Oluseun <a.davidoluseun@gmail.com>
2020-09-05 18:40:35 +0100

    Add Sweetalert library and PHP script to send mail

--- js/script.js
+++ js/script.js
@@ -55,19 +55,97 @@
         });
 
         // Bootstrap form validation
-        // Fetch the form to apply custom Bootstrap validation style to
+        // Fetch the form to apply custom Bootstrap validation style
         var forms = document.getElementsByClassName("contact-form");
 
         // Loop over it and prevent submission
         var validation = Array.prototype.filter.call(forms, function(form) {
             form.addEventListener("submit", function(event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add("was-validated");
+                event.preventDefault();
+                event.stopPropagation();
+                
+                // if (form.checkValidity() === true) {
+                    sendMail();
+                // }
+
+                // form.classList.add("was-validated");
             }, false);
         });
+
+        function sendMail() {
+            var $fullname = $("#fullname").val();
+            var $email = $("#email").val();
+            var $subject = $("#message-subject").val();
+            var $message = $("#message-body").val();
+
+            // For success/failure feedback
+            var $firstname = $fullname;
+  
+            // Check for white space in name for success/fail feedback
+            if ($firstname.indexOf(" ") >= 0) {
+                // Assign the firstname to variable $firstname
+                $firstname = $fullname.split(" ").shift();
+            }
+
+            // Disable send button until AJAX call is completed to prevent duplicate messages
+            var $sendBtn = $("#btn-send");
+            $sendBtn.prop("disabled", true).text("Sending...");
+
+            $.ajax({
+                url: "mail/contact.php",
+                type: "POST",
+                data: {
+                    fullname: $fullname,
+                    email: $email,
+                    subject: $subject,
+                    message: $message
+                },
+                cache: false,
+                success: successFn,
+                error: errorFn,
+                complete: completeFn
+            });
+
+            function successFn(response) {
+                console.log(response);
+                if (response == "Required") {
+                    swal({
+                        title: "Hi",
+                        text: "Your Fullname, valid email address and message are required!",
+                        icon: "warning",
+                        button: "Close"
+                    });
+                }
+
+                if (response == "Sent") {
+                    swal({
+                        title: "Hi " + $firstname,
+                        text: "Your message was sent successfully",
+                        icon: "success",
+                        button: "Close"
+                    });
+                }
+
+            }
+
+            function errorFn() {
+                // Error feedback
+                swal({
+                    title: "Hi " + $firstname,
+                    text: "It seems that my server is not responding. Please try again later!",
+                    icon: "error",
+                    button: "Close"
+                });
+            }
+
+            function completeFn() {
+                // Re-enable submit button when AJAX call is completed
+                setTimeout(function() {
+                    $sendBtn.prop("disabled", false).text("Send");
+                }, 1500);
+            }
+        }
+  
   
     }); // jQuery document ready function
 
--- mail/contact.php
+++ mail/contact.php
@@ -1,3 +1,39 @@
 <?php
+
+    // Get user's inputs
+    $fullname = $_POST["fullname"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // if inputs are empty or email not valid
+    if (empty($fullname) || empty($email) || 
+        empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        // Output "Required" and terminate the script   
+        exit("Required");
+    }
+
+    // Convert some HTML tags in inputs to HTML Entities
+    $fullname = htmlspecialchars($fullname);
+    $email = htmlspecialchars($email);
+    $subject = htmlspecialchars($subject);
+    $message = htmlspecialchars($message);
+
+    // Declare mail parameters
+    $to = "o.d.anifowose@gmail.com"; 
+    $email_subject = "New Entry: Debbs Portfolio Website.";
+    $email_body = "Name:" . "\n" . $fullname . "\n\n" . "Email:" . "\n" . $email . "\n\n";
+    $email_body .= "Subject:" . "\n" . $subject . "\n\n" . "Message:" . "\n" . $message;
+
+    // use wordwrap() if lines are longer than 70 characters
+    $email_body = wordwrap($email_body, 70)
+    $headers = "From: debbs@portfolio.com" . "\n" . "Reply-To: " . $email; 
+
+    // Send mail
+    mail($to, $email_subject, $email_body, $headers);
+
+    // Output "Sent" and terminate the script after sending mail
+    exit("Sent")
        
 ?>
